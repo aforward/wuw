a1635e2fc7e34bbb9d29c39d179a80f5b3c74a92
Andrew Forward <aforward@gmail.com>
2017-02-04 15:19:28 -0500

    Add mix test.watch for continuous testing

--- apps/wuw_web/mix.exs
+++ apps/wuw_web/mix.exs
@@ -20,9 +20,13 @@ defmodule WuwWeb.Mixfile do
   #
   # Type `mix help compile.app` for more information.
   def application do
-    [mod: {WuwWeb, []},
-     applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext]]
+    [applications: applications(Mix.env),
+     mod: {WuwWeb, []}]
   end
+
+  defp applications(:dev), do: applications(:all) ++ [:mix_test_watch]
+  defp applications(:test), do: applications(:dev)
+  defp applications(_), do: [:logger, :cowboy, :ecto, :postgrex]
 
   # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "web", "test/support"]
@@ -35,8 +39,13 @@ defmodule WuwWeb.Mixfile do
     [{:phoenix, "~> 1.2.1"},
      {:phoenix_pubsub, "~> 1.0"},
      {:phoenix_html, "~> 2.6"},
+     {:phoenix_ecto, "~> 3.2.1"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
+     {:mix_test_watch, "~> 0.3", only: [:dev, :test]},
+     {:ex_doc, "~> 0.14", only: :dev},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:ecto, "~> 2.1.3"},
+     {:postgrex, ">= 0.13.0"}]
   end
 end
