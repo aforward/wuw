18604a9b43f1fe0cc85f27df249ca3015b551029
Andrew Forward <aforward@gmail.com>
2017-02-04 21:38:09 -0500

    Add basic welcome page

--- apps/wuw_web/test/controllers/page_controller_test.exs
+++ apps/wuw_web/test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule WuwWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Hello WuwWeb!"
+    assert html_response(conn, 200) =~ "What are you worth?"
   end
 end
